0e385e28ac4851fdd348f217ec929f473f40e72b
KeNnY <kenny_0@mail.ru>
2022-09-25 13:16:38 +0300

    add 2 seminar

---
+++ seminars/seminar_2.py
@@ -0,0 +1,24 @@
+# task 1
+
+# number = int(input("Enter the number "))
+# num = 1
+# for i in range(number):
+#     print(num, end=" ")
+#     num *= -3
+
+# task 2
+
+# n = int(input('Введите число: '))
+# ls = list()
+
+# for k in range(1,n+1):
+#     ls.append(3*k+1)
+
+# print(ls)
+
+# task 3
+
+# str1 = input("Enter the string ")
+# under_str = input("Enter the under string")
+
+# print(str1.count(under_str))
